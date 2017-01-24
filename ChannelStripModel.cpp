887db6958bb82757543d8474383be8d7232d3eb6
witte <witte@tuta.io>
2017-01-24 03:36:46 -0200

    A lot of modifications, we're now using the model/view approach to create, manage and present the channels. It's a lot easier to set them up from main.cpp and in the near future to load them from an external file, and it's also performing very well.

---
+++ ChannelStripModel.cpp
@@ -0,0 +1,6 @@
+#include "ChannelStripModel.h"
+
+ChannelStripModel::ChannelStripModel()
+{
+
+}
---
+++ ChannelStripModel.h
@@ -0,0 +1,11 @@
+#ifndef CHANNELSTRIPMODEL_H
+#define CHANNELSTRIPMODEL_H
+
+
+class ChannelStripModel
+{
+public:
+    ChannelStripModel();
+};
+
+#endif // CHANNELSTRIPMODEL_H
